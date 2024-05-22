eaf45a4a387b8b788bb219c687f95f57b51950b8
fred <fredy.rodriguez@pucp.pe>
2024-05-22 17:39:36 -0500

    feat: enable CORS request

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,20 @@ import express, { Request, Response } from "express";
 import { gamerRouter } from "./router/gamer.router";
 import { wordRouter } from "./router/word.router";
 import scheduleDailyWordUpdate from "./services/scheduler.service";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ ok: "successfuly loading index page" });
 });
 app.use(gamerRouter);
 app.use(wordRouter);
-scheduleDailyWordUpdate()
+scheduleDailyWordUpdate();
 
 app.listen(PORT, () => {
   console.log(`Server running at ${PORT}`);
